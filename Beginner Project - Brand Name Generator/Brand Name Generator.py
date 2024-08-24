5f7517d0bb13109a97cad5c4c56ae9bc542447ba
Mathe David <mathew.david1002@gmail.com>
2024-08-24 06:29:16 +0530

    Add files via upload

---
+++ Beginner Project - Brand Name Generator/Brand Name Generator.py
@@ -0,0 +1,6 @@
+# Beginner Project - Brand Name Generator
+
+city_grew = input("Mention the name of the city in which you grew: ")
+pet_name = input("What is the name of your pet: ")
+
+print("The suggested band name for your band is : ",city_grew + " " + pet_name)
